ab4c9106391a91b5b8def308ed8e7c6014d86325
Michal Hlavac <michal.hlavac@xit.camp>
2020-05-22 21:10:57 +0200

    name refactoring

--- src/main/java/camp/xit/google/api/credentials/GoogleCredentials.java
+++ src/main/java/camp/xit/google/api/credentials/GoogleCredentials.java
@@ -1,4 +1,4 @@
-package camp.xit.google.api.serviceaccount;
+package camp.xit.google.api.credentials;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
